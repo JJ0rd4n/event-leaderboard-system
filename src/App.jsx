8165fad0eda80bdd93da2667398b209a5f199241
JJ0rd4n <jamonj@protonmail.com>
2023-06-21 11:51:34 +0200

    feat: fetch data from db

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react";
+import supabaseClient from "./server/supabaseClient";
+
 function App() {
+  const [events, setEvents] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  async function fetchEvents() {
+    const { data, error } = await supabaseClient.from("events").select();
+
+    if(error){
+      setFetchError(error)
+      setEvents([])
+      console.log(error.message)
+    }
+    if(data){
+      setEvents(data);
+      setFetchError(null)
+    }
+    
+  }
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <main className="container mx-auto">
+      {fetchError && (<p>{fetchError.message} : {fetchError.hint}</p>)}
+      <ul>
+        {events.map((event) => (
+          <li key={event.eventName}>{event.eventName}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
-export default App
+export default App;
